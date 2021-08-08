78215b4adea77bccedf143a5bc6e507d0ba7e452
Faishal Murtadho <muhammad.faishal@myboost.id>
2021-08-08 13:09:30 +0700

    Mock add button in ContactListPage header

--- app/Navigation/RootNavigator/RootNavigator.js
+++ app/Navigation/RootNavigator/RootNavigator.js
@@ -1,10 +1,31 @@
+import React from 'react';
 import { createAppContainer, createStackNavigator } from 'react-navigation';
+import { Text, TouchableOpacity } from 'react-native';
+
 import ContactListPage from '../../Containers/ContactListPage/ContactListPage.container';
 import ContactDetailPage from '../../Containers/ContactDetailPage/ContactDetailPage.container';
+import { Routes } from '../index';
 
 const MainStackNavigator = createStackNavigator({
-  ContactListPage: { screen: ContactListPage },
-  ContactDetailPage: { screen: ContactDetailPage },
+  ContactListPage: {
+    screen: ContactListPage,
+    navigationOptions: ({ navigation }) => ({
+      title: 'Contact',
+      headerRight: (
+        <TouchableOpacity
+          onPress={() => navigation.navigate(Routes.MainStackNavigator.ContactDetailPage)}
+        >
+          <Text>+</Text>
+        </TouchableOpacity>
+      ),
+    }),
+  },
+  ContactDetailPage: {
+    screen: ContactDetailPage,
+    navigationOptions: ({
+      title: 'Contact Detail',
+    }),
+  },
 });
 
 export const RootNavigator = createAppContainer(MainStackNavigator);
