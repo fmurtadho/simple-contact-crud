949d30ff1ddd30fde57964da1780cbb9863bc98c
Faishal Murtadho <muhammad.faishal@myboost.id>
2021-08-08 10:59:19 +0700

    Render basic structure of ContactDetailPage

--- app/Containers/ContactDetailPage/ContactDetailPage.component.js
+++ app/Containers/ContactDetailPage/ContactDetailPage.component.js
@@ -1,33 +1,94 @@
 import React, { Component } from 'react';
 import {
   View,
-  Text,
+  Image,
+  TextInput,
 } from 'react-native';
+import {
+  func, shape,
+} from 'prop-types';
 
-import { Styles } from '../ContactListPage/ContactListPage.component.style';
+import { Styles } from './ContactDetailPage.component.style';
 
 class ContactDetailPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      id: '',
       firstName: '',
       lastName: '',
+      age: '',
+      photo: '',
     };
   }
 
   componentDidMount() {
-
+    return this.setData();
   }
 
+  setData = () => {
+    const { navigation: { getParam } } = this.props;
+    const item = getParam('item', 'NO-ITEM');
+
+    if (item !== 'NO-ITEM') {
+      this.setState({
+        ...item,
+      });
+    }
+  };
+
+  renderImage = () => {
+    const { photo } = this.state;
+    if (photo) {
+      return (
+        <Image
+          style={Styles.photo}
+          source={{ uri: photo }}
+        />
+      );
+    }
+
+    return <View style={Styles.photo} />;
+  };
+
   render() {
+    const {
+      id,
+      firstName,
+      lastName,
+      age,
+      photo,
+    } = this.state;
+
     return (
       <View style={Styles.container}>
-        <Text>
-          ContactDetailPage
-        </Text>
+        {this.renderImage()}
+        <TextInput
+          style={Styles.textInput}
+          value={id}
+        />
+        <TextInput
+          style={Styles.textInput}
+          value={firstName}
+        />
+        <TextInput
+          value={lastName}
+        />
+        <TextInput
+          value={age.toString()}
+        />
+        <TextInput
+          value={photo}
+        />
       </View>
     );
   }
 }
+
+ContactDetailPage.propTypes = {
+  navigation: shape({
+    getParam: func.isRequired,
+  }),
+};
 
 export { ContactDetailPage };
--- app/Containers/ContactDetailPage/ContactDetailPage.component.style.js
+++ app/Containers/ContactDetailPage/ContactDetailPage.component.style.js
@@ -1,7 +1,20 @@
+import { Colors } from '../../Theme';
+
 const Styles = {
   container: {
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  photo: {
+    height: 200,
+    width: 200,
+    borderRadius: 8,
+    borderWidth: 0.5,
+    borderColor: Colors.BLACK,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: Colors.BLACK
   },
 };
 
